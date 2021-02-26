ccb2ae4528e4dfd319d07ab154bc2c9edbbd9571
Lina <linakherchi@gmail.com>
2021-02-26 13:24:56 -0800

    Change name of entry file for better readability

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: "./calculator.jsx",
+  entry: "./entry_file.jsx",
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
