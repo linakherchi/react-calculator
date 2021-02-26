21d25c25f221d5b3c2b4b503035603c76c4b6699
Lina <linakherchi@gmail.com>
2021-02-26 15:06:09 -0800

    Setting up operator, custom and number buttons

--- components/calculator.jsx
+++ components/calculator.jsx
@@ -7,20 +7,45 @@ export default class Calculator extends React.Component{
     }
     render(){
         const operators = ["÷", "x", "-", "+", "="];
-        const customButtons = ["AC", "+/-", "%"]
+        const customButtons = ["AC", "+/-", "%"];
+        const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
         return(
             <section className="calculator">
 
                 <div className="calculator-screen">{this.state.currentNumberShownInCalculator}</div>
 
                 <div className="calculator-board">
-                    <ul>
+                    <ul className="operators-section">
                         {operators.map((operator) => {
                             return (
                                 <OperatorButton operator={operator}/>
                             )
                         })}
                     </ul>
+
+                    <section className="non-operators-section">
+                        <ul>
+                            {customButtons.map((customButton) => {
+                                return (
+                                    <CustomButton customButton={customButton}/>
+                                )
+                            })}
+                        </ul>
+
+                        <ul>
+                            {numbers.map(number => {
+                                return (
+                                    <NumberButton number={number}/>
+                                )
+                            })}
+                        </ul>
+
+                        <ul>
+                            <button>0</button>
+                            <button>.</button>
+                        </ul>
+                    </section>
                 </div>
             </section>
         )
